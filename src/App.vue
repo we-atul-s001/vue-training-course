2db1f964cf30bd586a91189f84d7103bd1d79c49
Atul Pratap Singh <atul-s001@webreinvent.com>
2024-09-04 11:36:13 +0530

    Counter Component with help of option api

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,3 @@
 <template>
-    
+    <counter></counter>
 </template>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,9 +1,35 @@
 <template>
-    <div>
-        Counter Componenet
+  <div class="text-center">
+    Counter Componenet
 
-        <button @click.prevent="decrease">-</button>
-        <span>0</span>
-        <button>+</button>
-    </div>
+    <button @click.prevent="decreaseCounter()">-</button>
+    <span>{{ counter }}</span>
+    <button @click.prevent="increaseCounter()">+</button>
+  </div>
 </template>
+<style>
+.text-center {
+    text-align: center;
+    font-size: 25px;
+}
+</style>
+<script>
+export default{
+    data() {
+        return {
+            counter: 0
+        };
+    },
+    methods: {
+        decreaseCounter()
+        {
+            this.counter--;
+        },
+
+        increaseCounter()
+        {
+            this.counter++;
+        }
+    }
+};
+</script>
