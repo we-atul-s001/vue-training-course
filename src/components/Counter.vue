73c9dda4bcc66fbb12b98e93349b523a5df02e4d
Atul Pratap Singh <atul-s001@webreinvent.com>
2024-09-04 11:08:09 +0530

    Counter Component with option api

---
+++ src/components/Counter.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        Counter Componenet
+
+        <button @click.prevent="decrease">-</button>
+        <span>0</span>
+        <button>+</button>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import Counter from './components/Counter.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component('Counter', Counter);
+
+app.mount('#app');
